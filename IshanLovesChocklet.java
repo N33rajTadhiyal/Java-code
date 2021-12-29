cbe269fe398115f09bf430152072828619a82574
Neeraj Tadhiyal <tadhiyalne@gmail.com>
2021-12-29 23:24:39 +0530

    added the question

--- IshanLovesChocklet.java
+++ IshanLovesChocklet.java
@@ -1,9 +1,14 @@
 public class IshanLovesChocklet {
     public static void main(String[] args) {
+
+        /**
+         * As we know, Ishaan has a love for chocolates. He has bought a huge chocolate bar that contains N chocolate squares. Each of the squares has a tastiness level which is denoted by an array A[].
+Ishaan can eat the first or the last square of the chocolate at once. Ishaan has a sister who loves chocolates too and she demands the last chocolate square. Now, Ishaan being greedy eats the more tasty square first. 
+Determine the tastiness level of the square which his sister gets.
+         */
         int a[]={5,4,2,9};
-        int n = a.length;
         int i =0;
-        int j =n-1;
+        int j =a.length-1;
         int small=a[0];
         while(i<j)
         {
